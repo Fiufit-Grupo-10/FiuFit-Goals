04e98cc3c01eaf18355e6416e1b37b7a6b4b691a
axelkelman <akelman@fi.uba.ar>
2023-06-08 20:36:34 +0000

    Agrego post para info de entrenamientos

---
+++ app/api/trainings_info/models.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field
+from uuid import uuid4
+from datetime import datetime, time
+
+
+class Exercise(BaseModel):
+    name: str
+    category: str
+    amount: int
+    exercise_type: str
+    finished: bool
+    time: time
+    updated: datetime = Field(default_factory=datetime.utcnow)
+
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "name": "flexiones",
+                "category": "repeticiones",
+                "amount": "20",
+                "exercise_type": "Fuerza",
+                "finished": True,
+                "time": "01:10:15",
+            }
+        }
+
+
+class Training(BaseModel):
+    id: str = Field(default_factory=uuid4, alias="_id")
+    user_id: str = Field(...)
+    exercises: list[Exercise]
---
+++ tests/api/test_trainings.py
@@ -0,0 +1,19 @@
+def test_load_training(test_app):
+    exercises = [
+        {
+            "name": "flexiones",
+            "category": "repeticiones",
+            "amount": "20",
+            "exercise_type": "Fuerza",
+            "finished": True,
+            "time": "01:10:15",
+        }
+    ]
+
+    response = test_app.post(
+        "/users/c59710ef-f5d0-41ba-a787-ad8eb739ef4c/training_info", json=exercises
+    )
+    assert response.status_code == 201
+
+    body = response.json()
+    assert "_id" in body
