4f1c9fd84532346c312f0ff1349c0a72aed3d03d
axelkelman <66752570+axelkelman@users.noreply.github.com>
2023-06-09 17:33:13 -0300

    Update routes.py

--- app/api/trainings_info/routes.py
+++ app/api/trainings_info/routes.py
@@ -3,10 +3,10 @@ from app.api.trainings_info import crud
 from app.api.trainings_info.models import Exercise, Training
 
 
-router = APIRouter(tags=["trainings_info"])
+router = APIRouter(tags=["training"])
 
 
-@router.post("/users/{user_id}/training_info", response_model=Training, status_code=201)
+@router.post("/users/{user_id}/training", response_model=Training, status_code=201)
 async def load_training_info(user_id: str, exercises: list[Exercise], request: Request):
     new_training = await crud.load_user_training(
         user_id=user_id, exercises=exercises, request=request
