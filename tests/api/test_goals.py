39c1d0f2ce8274da6033760635a7a888409bfdb6
axelkelman <akelman@fi.uba.ar>
2023-06-09 19:08:45 +0000

    Agrego campo limit

--- tests/api/test_goals.py
+++ tests/api/test_goals.py
@@ -4,7 +4,14 @@ import pytest
 
 
 def test_create_goals(test_app):
-    goals = [{"training_type": "Cardio", "amount": 100, "goal_type": "calories"}]
+    goals = [
+        {
+            "training_type": "Cardio",
+            "amount": 100,
+            "goal_type": "calories",
+            "limit": "27-08-23",
+        }
+    ]
 
     response = test_app.post(
         "/users/c59710ef-f5d0-41ba-a787-ad8eb739ef4c/goals", json=goals
@@ -16,7 +23,14 @@ def test_create_goals(test_app):
 
 
 def test_create_goals_with_incorrect_type(test_app):
-    goals = [{"training_type": "Cardio", "amount": 100, "goal_type": "running"}]
+    goals = [
+        {
+            "training_type": "Cardio",
+            "amount": 100,
+            "goal_type": "running",
+            "limit": "27-08-23",
+        }
+    ]
 
     response = test_app.post(
         "/users/c59710ef-f5d0-41ba-a787-ad8eb739ef4c/goals", json=goals
@@ -27,8 +41,18 @@ def test_create_goals_with_incorrect_type(test_app):
 @pytest.mark.anyio
 async def test_update_goals(test_app):
     goals = [
-        {"training_type": "Cardio", "amount": 100, "goal_type": "calories"},
-        {"training_type": "Fuerza", "amount": 1000, "goal_type": "calories"},
+        {
+            "training_type": "Cardio",
+            "amount": 100,
+            "goal_type": "calories",
+            "limit": "27-08-23",
+        },
+        {
+            "training_type": "Fuerza",
+            "amount": 1000,
+            "goal_type": "calories",
+            "limit": "27-08-23",
+        },
     ]
 
     async with AsyncClient(app=app, base_url="http://test") as ac:
@@ -39,7 +63,12 @@ async def test_update_goals(test_app):
     # id = response.json()["_id"]
 
     updated_goals = [
-        {"training_type": "steps", "amount": 5000, "goal_type": "steps"},
+        {
+            "training_type": "steps",
+            "amount": 5000,
+            "goal_type": "steps",
+            "limit": "27-08-23",
+        },
     ]
 
     async with AsyncClient(app=app, base_url="http://test") as ac:
@@ -55,8 +84,18 @@ async def test_update_goals(test_app):
 @pytest.mark.anyio
 async def test_get_goals(test_app):
     goals = [
-        {"training_type": "Cardio", "amount": 100, "goal_type": "calories"},
-        {"training_type": "Fuerza", "amount": 1000, "goal_type": "calories"},
+        {
+            "training_type": "Cardio",
+            "amount": 100,
+            "goal_type": "calories",
+            "limit": "27-08-23",
+        },
+        {
+            "training_type": "Fuerza",
+            "amount": 1000,
+            "goal_type": "calories",
+            "limit": "27-08-23",
+        },
     ]
 
     async with AsyncClient(app=app, base_url="http://test") as ac:
